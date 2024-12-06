de900797244e8429c8691c6e1084cd73e331c4d9
Hajikana <hajikana14243@gmail.com>
2024-12-06 18:13:18 +0700

    week2

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,81 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
+int a = 0;
+int Button = 15;
+int ledPins[] = {23, 19, 18, 0, 4, 16};
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  pinMode(Button, INPUT_PULLDOWN);
+  for (int i = 0; i < 6; i++){
+    pinMode(ledPins[i],OUTPUT);
+  }
+  
 }
-
 void loop() {
-  // put your main code here, to run repeatedly:
-}
+  while (a == 0){
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
-}
+    digitalWrite(ledPins[0],HIGH);
+    digitalWrite(ledPins[1],LOW);
+    digitalWrite(ledPins[2],LOW);
+  
+    digitalWrite(ledPins[3],LOW);
+    digitalWrite(ledPins[4],LOW);
+    digitalWrite(ledPins[5],HIGH);
+    delay(1000);
+
+  
+    digitalWrite(ledPins[0],HIGH);
+    digitalWrite(ledPins[1],LOW);
+    digitalWrite(ledPins[2],LOW);
+  
+    digitalWrite(ledPins[3],LOW);
+    digitalWrite(ledPins[4],HIGH);
+    digitalWrite(ledPins[5],LOW);
+    delay(1000);
+
+
+    digitalWrite(ledPins[0],LOW);
+    digitalWrite(ledPins[1],LOW);
+    digitalWrite(ledPins[2],HIGH);
+  
+    digitalWrite(ledPins[3],HIGH);
+    digitalWrite(ledPins[4],LOW);
+    digitalWrite(ledPins[5],LOW);
+    delay(1000);
+
+
+    digitalWrite(ledPins[0],LOW);
+    digitalWrite(ledPins[1],HIGH);
+    digitalWrite(ledPins[2],LOW);
+  
+    digitalWrite(ledPins[3],HIGH);
+    digitalWrite(ledPins[4],LOW);
+    digitalWrite(ledPins[5],LOW);
+    delay(1000);
+
+    while(digitalRead(Button) == HIGH) a = 1;
+   }
+
+    while (a == 1){
+      digitalWrite(ledPins[0],LOW);
+      digitalWrite(ledPins[1],LOW);
+      digitalWrite(ledPins[2],LOW);
+  
+      digitalWrite(ledPins[3],LOW);
+      digitalWrite(ledPins[4],LOW);
+      digitalWrite(ledPins[5],LOW);
+      delay(1000);
+
+
+      digitalWrite(ledPins[0],HIGH);
+      digitalWrite(ledPins[1],HIGH);
+      digitalWrite(ledPins[2],HIGH);
+  
+      digitalWrite(ledPins[3],HIGH);
+      digitalWrite(ledPins[4],HIGH);
+      digitalWrite(ledPins[5],HIGH);
+      delay(1000);
+      while(digitalRead(Button) == HIGH) a = 0;
+    }
+    }
+  
